f00ed45e822eadc434d1d5a998b5a251496e4c2b
LalitaAng <lalita.angsuphanich@rwth-aachen.de>
2024-12-24 22:26:14 +0100

    split section for drinks and desserts

--- js/main.js
+++ js/main.js
@@ -30,3 +30,25 @@ document.querySelectorAll(".menu-tab").forEach((tab) => {
     document.getElementById(`${category}-menu`).classList.add("active");
   });
 });
+
+// lazy loading
+// document.addEventListener("DOMContentLoaded", function() {
+//   let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+//   if ("IntersectionObserver" in window) {
+//       let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+//           entries.forEach(function(entry) {
+//               if (entry.isIntersecting) {
+//                   let lazyImage = entry.target;
+//                   lazyImage.src = lazyImage.dataset.src;
+//                   lazyImage.classList.remove("lazy");
+//                   lazyImageObserver.unobserve(lazyImage);
+//               }
+//           });
+//       });
+
+//       lazyImages.forEach(function(lazyImage) {
+//           lazyImageObserver.observe(lazyImage);
+//       });
+//   }
+// });
